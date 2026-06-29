f64c84ad06c2aad092c2013ad9a50d9e362d5987
hafitzalfi-sketch <hafitzalfi@gmail.com>
2026-06-29 21:55:30 +0700

    Add files via upload

--- register.php
+++ register.php
@@ -1,14 +1,32 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
 include 'koneksi.php';
 
-// Memeriksa apakah data dikirim via POST
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if(isset($_POST['username'])) {
-        echo "Sukses: Data diterima untuk user " . $_POST['username'];
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $cek = mysqli_query($conn, "SELECT * FROM user WHERE username='$username'");
+    $cekEmail = mysqli_query($conn, "SELECT * FROM user WHERE email='$email'");
+
+    if(mysqli_num_rows($cek) > 0){
+        echo "username_kembar";
+    } else if(mysqli_num_rows($cekEmail) > 0){
+        echo "email_kembar";
     } else {
-        echo "Error: POST diterima tapi kunci 'username' tidak ditemukan. Isi POST: " . json_encode($_POST);
+        $input = mysqli_query($conn, "INSERT INTO user (nama, email, username, password) VALUES ('$nama', '$email', '$username', '$password')");
+        if($input){
+            echo "berhasil";
+        } else {
+            echo "gagal";
+        }
     }
 } else {
-    echo "Error: Bukan request POST";
+    echo "Data tidak lengkap";
 }
 ?>
